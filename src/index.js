c8a13bc0aa16b0d27cce1a2fd2c6f37d45ea1f2f
fairwind2k <fairwind@md2k.net>
2024-04-10 00:44:06 +0200

    add GET rout

--- src/index.js
+++ src/index.js
@@ -38,6 +38,30 @@ app.get('/hello', (req, res)=> {
   res.send(greet);
 });
 
+app.get('/courses/new', (req, res) => {
+  res.send('Course build');
+});
+
+app.get('/courses/:id', (req, res) => {
+  res.send(`Course ID: ${req.params.id}`);
+});
+
+app.get('/users/new', (req, res) => {
+  res.send('User created');
+});
+
+app.get('/users/:userId', (req, res) => {
+  res.send(`User ID: ${req.params.userId}`);
+});
+
+app.get('/courses/:courseId/lessons/:lessonId', (req, res) => {
+  res.send(`Course ID: ${req.params.courseId}; Lesson ID: ${req.params.lessonId}`);
+});
+
+app.get('/users/:userId/post/:postId', (req, res) => {
+  res.send(`User ID: ${req.params.userId}; Post ID: ${req.params.postId}`);
+});
+
 app.listen({ port }, () => {
   console.log(`Example app listening on port ${port}`);
 });
