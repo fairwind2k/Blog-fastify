406563d9a458540c63446c78c5661ff5dbdf50ee
fairwind2k <fairwind@md2k.net>
2024-05-14 22:50:48 +0200

    add form handler

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import pug from 'pug';
 import sanitize from 'sanitize-html';
 import getUsers from './fakeUsers.js';
 import getCompanies from './fakeCompanies.js';
+import getCourses from './fakeCourses.js';
 
 
 export default async () => {
@@ -15,6 +16,7 @@ export default async () => {
 
   const users = getUsers();
   const companies = getCompanies();
+  // const courses = getCourses();
 
   await app.register(view, { engine: { pug } });
 
@@ -30,24 +32,45 @@ export default async () => {
     res.send(greet);
   });
 
-  app.get('/users', (req, res) => res.view('src/views/users/index', { users }));
+  app.get('/users', (req, res) => {
+    const term = req.query.term;
+    let filtered;
+    if (term !== '' && term !== null) {
+      filtered = users.filter((user) => user.username.toLowerCase().includes(term));
+      console.log('1');
+    } else {
+      console.log('2');
+      res.view('src/views/users/index', { term, users: users });
+    }
+    const data = { term, users: filtered };
+    res.view('src/views/users/index', data);
+  });
     
   app.post('/users', (req, res) => {
-  res.send('POST /users');
+    res.send('POST /users');
   });
 
   app.get('/users/new', (req, res) => {
-    res.send('User created');
+    res.view('src/views/users/new');
   });
 
   // app.get('/users/:userId', (req, res) => {
   //   res.send(`User ID: ${req.params.userId}`);
   // });
 
+  // app.get('/users/:userId', (req, res) => {
+  //   const escapedId = sanitize(req.params.userId);
+  //   res.type('html');
+  //   res.send(`<h1>${escapedId}</h1>`);
+  // });
+
   app.get('/users/:userId', (req, res) => {
     const escapedId = sanitize(req.params.userId);
-    res.type('html');
-    res.send(`<h1>${escapedId}</h1>`);
+    const user = users.find(({ id }) => id === escapedId);
+    if (!user) {
+      return res.status(404).send('User not found');
+    }
+    return res.view('src/views/users/show', { user });
   });
 
   app.get('/users/:userId/post/:postId', (req, res) => {
@@ -62,26 +85,87 @@ export default async () => {
     res.send(data.domains);
   });
 
-  // app.get('/courses/new', (req, res) => {
-  //   res.send('Course build');
+  // const state = {
+  //   courses: [
+  //     {
+  //       id: 1,
+  //       title: 'JS: Массивы',
+  //       description: 'Курс про массивы в JavaScript',
+  //     },
+  //     {
+  //       id: 2,
+  //       title: 'JS: Функции',
+  //       description: 'Курс про функции в JavaScript',
+  //     },
+  //     {
+  //       id: 3,
+  //       title: 'JS: Объекты',
+  //       description: 'Курс про объекты в JavaScript',
+  //     },
+  //   ],
+  // };
+
+  const courses =
+   [
+      {
+        id: 1,
+        title: 'JS: Массивы',
+        description: 'Курс про массивы в JavaScript',
+      },
+      {
+        id: 2,
+        title: 'JS: Функции',
+        description: 'Курс про функции в JavaScript',
+      },
+      {
+        id: 3,
+        title: 'JS: Объекты',
+        description: 'Курс про объекты в JavaScript',
+      },
+    ];
+
+  app.get('/courses/new', (req, res) => {
+    res.send('Course build');
+  });
+
+  //  get all courses in one page:
+  //
+  // app.get('/courses', (req, res)=> {
+  //   //const data = state.courses;
+  //   const data = courses;
+  //   res.view('src/views/courses/index', { data });
   // });
 
-  // app.get('/courses/:id', (req, res) => {
-  //   const { id } = req.query;
-  //   const course = courses.find((item) => item.id.toString() === id);
-  //   if (!course) {
-  //     res.code(404).send({ message: 'Course not found' });
-  //     return;
-  //   }
-  //   const data = {
-  //     course,
-  //   };
-  //   res.view('src/views/courses/show', data);
-  // });
+  app.get('/courses', (req, res) => {
+    const term = req.query.term;
+    let selectedCourses;
+    if (term !== '') {
+      // Фильтруем курсы по term
+      selectedCourses = courses.filter((item) => item.title === term);
+    } else {
+      // Извлекаем все курсы, которые хотим показать
+      selectedCourses = courses.slice(0, 2);
+    }  
+    const data = { term, courses: selectedCourses };  
+    res.view('src/views/courses/index', data);
+  });
+  
+  app.get('/courses/:id', (req, res) => {
+    const escapedCourseId = sanitize(req.params.id);
+    // const course = state.courses.find((item) => item.id.toString() === escapedCourseId);
+    const course = courses.find((item) => item.id.toString() === escapedCourseId);
+    if (!course) {
+      return res.code(404).send({ message: 'Course not found' });    
+    }
+    const data = {
+      course,
+    };
+    return res.view('src/views/courses/show', data);
+  });
 
-  // app.get('/courses/:courseId/lessons/:lessonId', (req, res) => {
-  //   res.send(`Course ID: ${req.params.courseId}; Lesson ID: ${req.params.lessonId}`);
-  // });
+  app.get('/courses/:courseId/lessons/:lessonId', (req, res) => {
+    res.send(`Course ID: ${req.params.courseId}; Lesson ID: ${req.params.lessonId}`);
+  });
 
   return app;
 };
