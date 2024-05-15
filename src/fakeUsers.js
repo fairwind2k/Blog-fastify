15a1ce4e3fcafb6763acc5fa6e4f5d46bb5dfb81
fairwind2k <fairwind@md2k.net>
2024-05-16 00:19:09 +0200

    add func to get random id

--- src/fakeUsers.js
+++ src/fakeUsers.js
@@ -6,9 +6,13 @@ const createRandomUser = () => ({
   email: faker.internet.email(),
 });
 
+export const generateId = () => faker.string.uuid();
+
 export default () => {
   faker.seed(25);
   return faker.helpers.multiple(createRandomUser, {
-    count: 50,
+    count: 5,
   });
 };
+
+
