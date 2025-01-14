443633331c87d2cd69fc0701df82baf89ecef5e4
fairwind2k <fairwind@md2k.net>
2025-01-14 22:22:34 +0100

    refactor login route

--- src/routes/sessions.js
+++ src/routes/sessions.js
@@ -1,13 +1,13 @@
 import encrypt from './encrypt.js';
 
   // add a login route that returns a login page
-  instance.get('/login', (request, reply) => {
+  app.get('/login', (request, reply) => {
     reply.type('text/html')
     reply.send(loginPage())
   })
   
   // add a login route that handles the actual login
-  instance.post('/login', (request, reply) => {
+  app.post('/login', (request, reply) => {
     const { email, password } = request.body
   
     if (password === 'abcdef') {
