8f08180209a6cc5720e93aa30a288158b2042ecc
fairwind2k <fairwind@md2k.net>
2025-02-06 00:56:28 +0100

    add login page

--- src/routes/sessions.js
+++ src/routes/sessions.js
@@ -1,20 +1,62 @@
-import encrypt from './encrypt.js';
+// import encrypt from './encrypt.js';
 
-  // add a login route that returns a login page
-  app.get('/login', (request, reply) => {
-    reply.type('text/html')
-    reply.send(loginPage())
-  })
-  
-  // add a login route that handles the actual login
-  app.post('/login', (request, reply) => {
-    const { email, password } = request.body
-  
-    if (password === 'abcdef') {
-      request.session.authenticated = true
-      reply.type('text/html')
-      reply.send(defaultPage(true))
-    } else {
-      reply.redirect(401, '/login')
-    }
+const users = [
+  {
+    id: 1,
+    username: 'first user',
+    email: 'first@user.com',
+    password: 'user1'
+  },
+  {
+    id: 2,
+    username: 'second user',
+    email: 'second@user.com',
+    password: 'user2'
+  },
+];
+
+export default (app, db) => {
+
+  app.get('/login', (req, res) => {
+    const { username } = req.session;
+    res.view('src/views/sessions/login', { username });
   });
+
+  app.get('/sessions/new', (req, res) => {
+    res.view('src/views/sessions/new');
+  });
+
+  app.post('/sessions', (req, res) => {
+    const { username, password } = req.body;
+    const user = users.find(elem => elem.username === username);
+
+    // if (!user || (decrypt(user.password) !== password)) {
+    //   res.view('src/views/sessions/new', { error: 'Неверное имя пользователя или пароль'});
+    //   return;
+    // }
+    if ( !user || user.password !== password) {
+      res.view('src/views/sessions/new', { message: 'Неверное имя пользователя или пароль'});
+      return;
+    };
+    req.session.username = username;
+    res.redirect('/login');
+  });
+
+  // app.post('/sessions/delete', (req, res) => {
+  //   req.session.destroy( (err) => {
+  //     if (err) {
+  //       res.status(500);
+  //       res.send('Internal Server Error');
+  //     } else {
+  //       res.redirect('/login');
+  //     }
+  //   });
+  // });
+
+  app.post('/sessions/delete', (req, res) => {
+    req.session.delete();
+    // req.session = null;
+    return res.redirect('/login');
+  });
+  
+};
