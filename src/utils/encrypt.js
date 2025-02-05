b967baf0b74bbfe8bf7fb4ef3f17b80f286ca4d9
fairwind2k <fairwind@md2k.net>
2025-02-06 00:57:06 +0100

    update code

--- src/utils/encrypt.js
+++ src/utils/encrypt.js
@@ -1,7 +1,14 @@
-import crypto from 'crypto';
+// import crypto from 'crypto';
+import CryptoJS from 'crypto-js';
 
-export default (text) => {
-  const hash = crypto.createHmac('sha512', 'salt');
-  hash.update(text);
-  return hash.digest('hex');
-};
+// export default (text) => {
+//   const hash = crypto.createHmac('sha512', 'salt');
+//   hash.update(text);
+//   return hash.digest('hex');
+// };
+
+const key = 'secret phrase';
+
+export const encrypt = (password) => CryptoJS.AES.encrypt(password, key).toString();
+
+export const decrypt = (hash) => CryptoJS.AES.decrypt(hash, key).toString(CryptoJS.enc.Utf8);
