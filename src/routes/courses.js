d6e0c97cf18b4818a7a54384d89c21a8cad66f47
fairwind2k <fairwind@md2k.net>
2025-02-18 23:17:30 +0100

    add db to courses routes

--- src/routes/courses.js
+++ src/routes/courses.js
@@ -25,21 +25,6 @@ export default (app, db) => {
     },
   ];
 
-  // app.get("/courses", { name: "courses" }, (req, res) => {
-  //   const term = req.query.term;
-  //   let selectedCourses = courses;
-  //   if (term) {
-  //     // Фильтруем курсы по term
-  //     selectedCourses = courses.filter((item) =>
-  //       item.title.toLowerCase().includes(term.toLowerCase())
-  //     );
-  //   } else {
-  //     //   // Извлекаем все курсы, которые хотим показать
-  //     selectedCourses = courses.slice(0, 5);
-  //   }
-  //   res.view("src/views/courses/index", { courses: selectedCourses });
-  // });
-
   app.get("/courses", { name: "courses" }, (req, res) => {
     db.all('SELECT * FROM courses', (error, data) => {
       const templateData = {
@@ -47,6 +32,7 @@ export default (app, db) => {
         flash: res.flash(),
         error,
       };
+      console.log('templateData', templateData);
       res.view('src/views/courses/index', templateData);
     });
   });
@@ -55,7 +41,7 @@ export default (app, db) => {
     res.view('src/views/courses/new');
   });
 
-  app.get('/courses/:id', (req, res) => {
+  app.get('/courses/:id', { name: 'course' }, (req, res) => {
     const id = sanitize(req.params.id);
     if (!Number.isInteger(Number(id)) || id <= 0) {
       req.flash('warning', 'Некорректный идентификатор курса');
@@ -117,7 +103,7 @@ export default (app, db) => {
         title,
         description,
       };
-      
+
       const stmt = db.prepare('INSERT INTO courses(title, description) VALUES(?, ?)');
       stmt.run([course.title, course.description], function (error) {
         if (error) {
@@ -126,79 +112,64 @@ export default (app, db) => {
           res.redirect(app.reverse('newCourse'));
           return;
         }
+        // console.log('this.lastID', this.lastID);
         req.flash('success', 'Курс успешно создан');
-        res.redirect(`/courses/${this.lastID}`);
+        res.redirect(app.reverse('courses'));
       });
   });
  
-  // app.get("/courses/:id", (req, res) => {
-  //     const escapedCourseId = sanitize(req.params.id);
-  //     // const course = state.courses.find((item) => item.id.toString() === escapedCourseId);
-  //     const course = courses.find(
-  //     (item) => item.id.toString() === escapedCourseId
-  //     );
-  //     if (!course) {
-  //     return res.code(404).send({ message: "Course not found" });
-  //     }
-  //     const data = {
-  //     course,
-  //     };
-  //     return res.view("src/views/courses/show", data);
-  // });
-
-  // app.get('/courses/:id', (req, res) => {
-  //   const id = sanitize(req.params.id);
-  //   db.get(`SELECT * FROM courses WHERE id = ${id}`, (err, data) => {
-  //     const templateData = {
-  //       course: data,
-  //       error: err,
-  //     };
-  //     console.log('templateData:', templateData);
-  //     res.view("src/views/courses/show", templateData);
-  //   });
-  // });    
-
   app.get('/courses/:courseId/lessons/:lessonId', (req, res) => {
     res.send(`Course ID: ${req.params.courseId}; Lesson ID: ${req.params.lessonId}`);
   });
 
   // Form for en editing specific course:
-  app.get('/courses/:id/edit',  { name: 'editCourse' }, (req, res) => {
+  app.get('/courses/:id/edit', { name: 'editCourse' }, (req, res) => {
     const { id } = req.params;
-    const course = courses.find((item) => item.id === id);
-    if (!course) {
-      res.code(404).send({ message: 'Course not found' });
-    } else {
-      res.view('src/views/courses/edit', { course });
-    }
+    db.get(`SELECT * FROM courses WHERE id = ${id}`, (error, data) => {
+      if (error) {
+        req.flash('warning', 'Course not found');
+        res.redirect(app.reverse('courses'));
+        return;
+      }
+      const templateData = {
+        course: data,
+        flash: res.flash(),
+      };
+      res.view('src/views/courses/edit', templateData);
+    });
   });
 
   // Updating specific course:
   app.patch('/courses/:id', { name: 'updateCourse' }, (req, res) => {
     const { id } = req.params;
     const { title, description } = req.body;
-    const courseIndex = courses.findIndex((item) => item.id === id);
-    if (courseIndex === -1) {
-      res.code(404).send({ message: 'User not found' });
-    } else {
-      courses[courseIndex] = { ...courses[courseIndex], title, description };
-      // in this sample we need convert number to string again:
-      // const stringId = id.toString();
-      res.redirect(app.reverse('updateCourse', { id }));
-    }
+    const stmt = db.prepare('UPDATE courses SET title = ?, description = ? WHERE id = ?');
+    stmt.run([title, description, id], function (error) {
+      if (error) {
+        console.error('Database error:', error);
+        req.flash('warning', 'Ошибка обновления курса');
+        res.redirect(app.reverse('course', { id }));
+        return;
+      }
+      req.flash('success', 'Курс успешно обновлен');
+      res.redirect(app.reverse('courses'));
+    });   
   });
-
+  
   // Deleting a specific course:
   app.delete('/courses/:id', { name: 'deleteCourse' }, (req, res) => {
     const { id } = req.params;
-    const courseIndex = courses.findIndex((item) => item.id === id);
-    if (courseIndex === -1) {
-      res.code(404).send({ message: 'Course not found' });
-    } else {
-      courses.splice(courseIndex, 1);
+    const stmt = db.prepare('DELETE FROM courses WHERE id = ?');
+    stmt.run([id], function (error) {
+      if (error) {
+        console.error('Database error:', error);
+        req.flash('warning', 'Ошибка удаления курса');
+        res.redirect(app.reverse('course', { id }));
+        return;
+      }
+      req.flash('success', 'Курс успешно удален');
       res.redirect(app.reverse('courses'));
-    }
+    });
   });
-
     
 };
